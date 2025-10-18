14eb33e85a1a26fb9b49a0338c8ade6427636e01
saiyed Rahil <rahilsaiyed1711@gmail.com>
2025-10-18 13:25:00 +0530

    fix the route issue

--- src/routes/api.routes.ts
+++ src/routes/api.routes.ts
@@ -1,14 +1,14 @@
-import express from "express"
-import userRouter from "./user.route"
-import incomeRouter from "./income.routes"
-import expenseRouter from './expense.routes'
-import bankAccountRouter from './bankaccount.routes';
-const app =express();
+import { Router } from "express";
+import userRouter from "./user.route";
+import incomeRouter from "./income.routes";
+import expenseRouter from "./expense.routes";
+import bankAccountRouter from "./bankaccount.routes";
 
+const router = Router();
 
-app.use("/user", userRouter);
-app.use("/income", incomeRouter);
-app.use("/expense",expenseRouter);
-app.use("/bankaccount",bankAccountRouter);
+router.use("/user", userRouter);
+router.use("/income", incomeRouter);
+router.use("/expense", expenseRouter);
+router.use("/bankaccount", bankAccountRouter);
 
-export default app;
+export default router;
